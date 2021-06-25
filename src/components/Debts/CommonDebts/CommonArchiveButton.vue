cc6e9c57ced81bd47013b5b85c5ee469584ffea4
manjaro <khudav.tigran@gmail.com>
2021-06-25 07:20:08 +0400

    add archive

---
+++ src/components/Debts/CommonDebts/CommonArchiveButton.vue
@@ -0,0 +1,107 @@
+<template>
+  <li
+    class="
+      bg-part
+      rounded-8
+      w-full
+      max-w-23
+      py-9
+      border-0
+      mb-10
+      ls-none
+      d-flex
+      justify-content-center
+      year
+    "
+    role="button"
+  >
+    <ul class="position-absolute shadow-1 px-10 w-full max-w-29 left-28 mounts">
+      <li class="ls-none my-5" v-for="item in mounts" :key="item.id">
+        <span
+          @click="$emit('getMount', item)"
+          class="text-gray-500 hov"
+          role="button"
+          >{{ item.mount }}</span
+        >
+      </li>
+    </ul>
+    <span class="fs-10 fw-600 text-white"><slot /></span>
+  </li>
+</template>
+
+<script>
+import CommonPartners from "@/common/CommonPartners.vue";
+export default {
+  components: { CommonPartners },
+  data() {
+    return {
+      mounts: [
+        {
+          id: 1,
+          mount: "Հունվար",
+        },
+        {
+          id: 2,
+          mount: "Փետրվար",
+        },
+        {
+          id: 3,
+          mount: "Մարտ",
+        },
+        {
+          id: 4,
+          mount: "Ապրիլ",
+        },
+        {
+          id: 5,
+          mount: "Մարտ",
+        },
+        {
+          id: 6,
+          mount: "Հունիս",
+        },
+        {
+          id: 7,
+          mount: "Հուլիս",
+        },
+        {
+          id: 8,
+          mount: "Օգոստոս",
+        },
+        {
+          id: 9,
+          mount: "Սեպտեմբեր",
+        },
+        {
+          id: 10,
+          mount: "Հոկտեմբեր",
+        },
+        {
+          id: 11,
+          mount: "Նոյեմբեր",
+        },
+        {
+          id: 12,
+          mount: "Դեկտեմբեր",
+        },
+      ],
+    };
+  },
+};
+</script>
+<style scoped>
+.mounts {
+  display: none;
+}
+.year:hover {
+  max-width: 9rem;
+  transition: all 0.3s ease;
+}
+.year:hover > .mounts {
+  display: block;
+}
+.hov:hover {
+  color: rgb(80, 80, 80);
+  font-weight: 600;
+}
+</style>
---
+++ src/components/Debts/SubjectDay/Archive.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="px-15">
+    <div class="mb-10" role="button">
+      <span class="fs-10 fw-600 text-gray-400">Արխիվ</span>
+    </div>
+    <div>
+      <ul class="ps-0">
+        <common-archive-button @getMount="getMount">2021</common-archive-button>
+        <common-archive-button>2020</common-archive-button>
+      </ul>
+      <div>
+        <div class="days-grid"></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import CommonArchiveButton from "../CommonDebts/CommonArchiveButton.vue";
+export default {
+  components: { CommonArchiveButton },
+  methods: {
+    getMount(item) {
+      console.log(item);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.days-grid {
+  display: grid;
+}
+</style>
